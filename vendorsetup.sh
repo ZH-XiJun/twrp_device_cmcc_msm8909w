709c86e23acb9e3aeaecd20bda64c15d61e986fc
wt_bruhhhh <59949135+ZH-XiJun@users.noreply.github.com>
2023-12-03 21:24:30 +0800

    Initial commit

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_msm8909w-user
+add_lunch_combo omni_msm8909w-userdebug
+add_lunch_combo omni_msm8909w-eng
